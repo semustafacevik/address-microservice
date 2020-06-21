a168217d8d8adccbbe9172f89bd2d0a966ae3ef3
MustafaCEVIK <cevikksoft@gmail.com>
2020-06-22 01:27:57 +0300

    Add src folder, add app.js, edit scripts -> package.json .

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.status(200).send(" ----  Address Microservice  ---- ");
+})
+
+app.use("", (req, res) => {
+    res.status(404).send("Opps! 404 Not Found.");
+})
+
+
+const localDBConnection = "mongodb://localhost/AddressDB";
+// const cloudDBConnection = process.env.DB_CONNECTION;
+
+mongoose.connect(localDBConnection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}, (error) => {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log("Connected to DB.");
+    }
+}
+);
+
+app.listen(process.env.PORT || 4040, () => {
+    console.log("App Started.");
+})
