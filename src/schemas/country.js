97254d79ba8e8ad9a1e7186b8c4fd0f1e6e43b80
MustafaCEVIK <cevikksoft@gmail.com>
2020-06-22 01:35:15 +0300

    Add schemas folder, generate models (country, province, district).

---
+++ src/schemas/country.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const countrySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 50,
+    }
+});
+
+module.exports = mongoose.model("Country", countrySchema);
---
+++ src/schemas/district.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const districtSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 50,
+    },
+    provinceId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Province',
+        required: true,
+    }
+});
+
+module.exports = mongoose.model("District", districtSchema);
